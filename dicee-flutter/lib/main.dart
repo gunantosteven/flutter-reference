49ac930a06697933689a63d8d004372c3f0fd144
Steven Gunanto <>
2020-05-19 00:59:57 +0700

    Add Dicee project with detecting user interaction

---
+++ dicee-flutter/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('Right button got pressed.');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
