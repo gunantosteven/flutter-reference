123d523796cd0d3335f6d4b28aebbd30ce526848
Steven Gunanto <>
2020-05-26 08:41:24 +0700

    Add lifecycle method

--- Navigation-Flutter-Demo/lib/screen2.dart
+++ Navigation-Flutter-Demo/lib/screen2.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState called');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactive called');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build called');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
