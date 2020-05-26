ac16a554589493831bb77ab9d4a739db5eb3ad44
Steven Gunanto <>
2020-05-26 11:42:46 +0700

    Add Clima-Flutter project

---
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,53 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+
+      var decodeData = jsonDecode(data);
+
+      int condition = decodeData['weather'][0]['id'];
+      double temperature = decodeData['main']['temp'];
+      String cityName = decodeData['name'];
+
+      print(condition);
+      print(temperature);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold();
+  }
+}
